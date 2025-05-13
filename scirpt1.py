2f2d5168d749325877e3e639850396b87438ef58
Arsham Zare <arshamzare@icloud.com>
2025-05-13 11:43:11 -0400

    Simple Calculator

--- scirpt1.py
+++ scirpt1.py
@@ -1,2 +1,19 @@
 # Simple Calculator
 # Build a calculator that asks for two numbers and an operation (+, -, *, /), then prints the result.
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+if operation == "+":
+    print(num1 + num2)
+
+if operation == "-":
+    print(num1 - num2)
+
+if operation == "*":
+    print(num1*num2)
+
+if operation == "/":
+    print(num1/num2)
