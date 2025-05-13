de5629970fbf0ae4ec56e17b4699bdcf44d590fb
Arsham Zare <arshamzare@icloud.com>
2025-05-13 12:25:58 -0400

    adding the third project

---
+++ script3.py
@@ -0,0 +1,8 @@
+# Multiplication Table Generator
+# Ask the user for a number and print its multiplication table up to 10.
+
+num = int(input("What's your number:"))
+
+for i in range(1, 11):
+    print(i*num)
+    
